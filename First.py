c405cfadc3e3379b431ff1a5f63b785b7cb66769
Nikhil <nikhil.ahlawat@yahoo.com>
2025-09-29 14:00:53 +0100

    CREATE: Added my new pycharm file

---
+++ First.py
@@ -0,0 +1,29 @@
+# ! /usr/bin/env python3     SHEBANG
+# Author: Nikhil Ahlawat
+# Version: 1.0
+# Description: This is our first
+# Script
+
+"""
+DOCSTRING: This is multi line comment
+"""
+
+#
+name = input("Enter your name: ")
+
+print("My name is", name, sep=" ")
+
+
+import math
+print("Cosine of 0.5 =", math.cos(0.5))
+
+
+import random
+
+print("My lucky number is", random.randint(1, 50))
+
+
+
+
+
+print("DONE")
