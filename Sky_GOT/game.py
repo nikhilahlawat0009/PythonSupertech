7b87117e9a6eeaae4891eae6761adeff44b83b85
Nikhil <nikhil.ahlawat@yahoo.com>
2025-10-01 17:18:45 +0100

    Create: oops concepts

--- Sky_GOT/game.py
+++ Sky_GOT/game.py
@@ -1,12 +1,11 @@
-# ! /usr/bin/env python3
-# Author: NAhlawat
+#! /usr/bin/env python3
+# Author: DCameron
 # Description: This is a highly realistic online game with Tanks!
 """
     GOT - Game of Tanks for Sky employees
 """
 import sys
 from Sky_GOT.app.tank import Tank
-
 
 # import tank
 
@@ -27,6 +26,11 @@ def main():
     # And success..
     sage_tank.take_damage(59)
     arash_tank.take_damage(22)
+
+    # And now for some VISUALS!
+    print(f"Health of Sage's Tank is {sage_tank._health}")
+
+    print(f"Health of Sage's and Arash's Tanks: {sage_tank + arash_tank}")
 
     return None
 
